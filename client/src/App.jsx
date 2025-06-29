795d4ac9d7576ea0ac2e344cd685c74fe634bdee
imadityagolu <adityasng420.ak@gmail.com>
2025-06-29 23:02:17 +0530

    updated pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-  return (
-    <>
-      <p className='text-red-500'>working</p>
-    </>
-  )
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      
+    </Routes>
+  </BrowserRouter>
 }
-
-export default App
