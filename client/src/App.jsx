988949f294c09cdfdbdd47abffbe5aba96960563
imadityagolu <adityasng420.ak@gmail.com>
2025-07-01 21:12:28 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 
 import './App.css'
+import Home from './pages/Home';
+import SignUp from './pages/Signup';
+import SignIn from './pages/Signin';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return <BrowserRouter>
     <Routes>
-      
+      <Route path="/" element={<Home/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
     </Routes>
   </BrowserRouter>
 }
