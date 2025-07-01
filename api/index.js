a7c98c6e3fd2101c63cbb5d59d70278d4c8bcf66
imadityagolu <adityasng420.ak@gmail.com>
2025-07-01 22:13:52 +0530

    db connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGOODB_URL)
+.then( () => {console.log("MongoDb connected")})
+.catch( (error) => {console.log(error)});
 
 app.listen(3000, () => {
     console.log('server is up');
